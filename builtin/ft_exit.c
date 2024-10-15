d7dfd0c417b1f51f40f887e8b6887b9a1ff1dc53
1darko <dakojic@student.42.fr>
2024-10-15 14:22:24 +0200

    fixed args for exit

--- builtin/ft_exit.c
+++ builtin/ft_exit.c
@@ -6,11 +6,54 @@
 /*   By: dakojic <dakojic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:17:13 by dakojic           #+#    #+#             */
-/*   Updated: 2024/10/15 13:56:40 by dakojic          ###   ########.fr       */
+/*   Updated: 2024/10/15 14:22:07 by dakojic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+
+static long long ft_atol(char *str)
+{
+    long long ret;
+    int i;
+    int neg;
+
+	i = 0;
+	neg = 1;
+	ret = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		ret = ret * 10 + (str[i] - 48);
+		i++;
+	}
+	return (ret * neg);
+}
+
+static long long ret_exit(char *x)
+{
+    int i;
+
+    i = 0;
+    while(x[i] != '\0')
+    {
+        if(!(x[i] >= '0' && x[i] <= '9'))
+        {
+            printf("Error: Invalid exit argument. Numeric argument required.\n");
+            return (2);
+        }
+        i++;
+    }
+    return (ft_atol(x));
+}
 
 void ft_exit(t_execs *ex)
 {
@@ -18,7 +61,7 @@ void ft_exit(t_execs *ex)
 
     ret = 0;
     if(((t_execcmd *)ex->cmd)->args[1])
-        ret = (unsigned int)((t_execcmd *)ex->cmd)->args[1];
+        ret = (unsigned int)ret_exit(((t_execcmd *)ex->cmd)->args[1]);
     ft_listfree(ex->fds, fdsfree);
     execfree(ex);
     write(2,"exit\n",5);
