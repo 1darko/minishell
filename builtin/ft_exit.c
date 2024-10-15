d066c59e414b45ea7c87fe0a827b0cd91269bbba
1darko <dakojic@student.42.fr>
2024-10-15 13:55:57 +0200

    added exit code from arg

--- builtin/ft_exit.c
+++ builtin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: dakojic <dakojic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:17:13 by dakojic           #+#    #+#             */
-/*   Updated: 2024/10/14 16:52:38 by dakojic          ###   ########.fr       */
+/*   Updated: 2024/10/15 13:55:40 by dakojic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 void ft_exit(t_execs *ex)
 {
+    unsigned int ret;
+
+    ret = 0;
+    if(((t_execcmd *)ex->cmd)->args[1])
+        ret = (unsigned int)((t_execcmd *)ex->cmd)->args[1];
     ft_listfree(ex->fds, fdsfree);
     execfree(ex);
     write(1,"exit\n",5);
-    exit(0);
+    exit(ret);
 }
