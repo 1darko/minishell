7447a20112bdc1315a3af5d92d2b491dcb3abc69
1darko <dakojic@student.42.fr>
2024-10-17 12:36:22 +0200

    passing to static whats needed

--- tokens.c
+++ tokens.c
@@ -6,11 +6,60 @@
 /*   By: dakojic <dakojic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:03:20 by dakojic           #+#    #+#             */
-/*   Updated: 2024/09/26 13:04:18 by dakojic          ###   ########.fr       */
+/*   Updated: 2024/10/17 11:17:21 by dakojic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*other_token(char *temp, int *check)
+{
+	char	a;
+
+	a = *temp;
+	if (!*temp)
+		*check = 0;
+	else
+		*check = 'a';
+	if (a == '\"' || a == '\'')
+	{
+		temp++;
+		while (temp && *temp != a)
+			temp++;
+		temp++;
+		if (*temp == '\'' || *temp == '\"')
+			other_token(temp, check);
+	}
+	else
+		while (*temp && !ft_strchr(" \t\n\r\v<>()|&", *temp))
+			temp++;
+	return (temp);
+}
+
+static void	extratoken(char **temp, int *check)
+{
+	if (*check == '|' && *(*temp) == '|')
+	{
+		(*temp)++;
+		*check = 'o';
+	}
+	else if (*check == '&' && *(*temp) == '&')
+	{
+		(*temp)++;
+		*check = 'a';
+	}
+	else if (*check == '>' && *(*temp) == '>')
+	{
+		(*temp)++;
+		*check = '+';
+	}
+	else
+	{
+		(*temp)++;
+		*check = 'h';
+	}
+}
+
 
 int	gettoken(char **ptr, char **ptr_token, char **ptr_endtoken)
 {
@@ -35,52 +84,4 @@ int	gettoken(char **ptr, char **ptr_token, char **ptr_endtoken)
 		*ptr_endtoken = temp;
 	*ptr = temp;
 	return (check);
-}
-
-char	*other_token(char *temp, int *check)
-{
-	char	a;
-
-	a = *temp;
-	if (!*temp)
-		*check = 0;
-	else
-		*check = 'a';
-	if (a == '\"' || a == '\'')
-	{
-		temp++;
-		while (temp && *temp != a)
-			temp++;
-		temp++;
-		if (*temp == '\'' || *temp == '\"')
-			other_token(temp, check);
-	}
-	else
-		while (*temp && !ft_strchr(" \t\n\r\v<>()|&", *temp))
-			temp++;
-	return (temp);
-}
-
-void	extratoken(char **temp, int *check)
-{
-	if (*check == '|' && *(*temp) == '|')
-	{
-		(*temp)++;
-		*check = 'o';
-	}
-	else if (*check == '&' && *(*temp) == '&')
-	{
-		(*temp)++;
-		*check = 'a';
-	}
-	else if (*check == '>' && *(*temp) == '>')
-	{
-		(*temp)++;
-		*check = '+';
-	}
-	else
-	{
-		(*temp)++;
-		*check = 'h';
-	}
 }
